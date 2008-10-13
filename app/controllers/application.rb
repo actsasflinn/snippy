be325915326a8c72fba48c14983ec00d784a7ec7
Flinn <flinn@actsasflinn.com>
2008-10-13 14:12:49 -0400

    only show 10 tags on front

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -16,7 +16,7 @@ class ApplicationController < ActionController::Base
   before_filter :stats
 
   def stats
-    @top_tags = Tag.with_snippets.paginate(:page => 1, :order => 'taggings_count desc')
+    @top_tags = Tag.with_snippets.paginate(:page => 1, :per_page => 10, :order => 'taggings_count desc')
     @top_languages = Language.with_snippets.paginate(:page => 1, :conditions => "snippets_count > 0", :order => 'snippets_count desc')
   end
 end
